b4d675cdca8bbabf6cf8fed08f6798a06ae240c4
masrur-sakib <mashrur333@gmail.com>
2021-10-11 12:07:10 +0600

    Login page responsive issue fixed.

--- src/components/AuthenticationPage/Login/Login.js
+++ src/components/AuthenticationPage/Login/Login.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = (props) => {
@@ -17,62 +16,65 @@ const Login = (props) => {
   } = props;
 
   return (
-    <div className="login-section">
-      <div className="login-section">
-        {hasAccount ? (
-          <h4 className="login-title">Sign In</h4>
-        ) : (
-          <h4 className="login-title">Sign Up</h4>
-        )}
-        <br />
-        {/* <input type="number" name="phone" id="" placeholder="Phone Number" /> */}
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          autoFocus
-          required
-        />
-        <p className="authentication-error-message"> {emailError} </p>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        <p className="authentication-error-message"> {passwordError} </p>
-        <p>
-          We'll call or text you to confirm your number. Standard message and
-          data rates apply.
-        </p>
-        <div className="button-container">
-          {hasAccount ? (
-            <>
-              <button className="login-button" onClick={handleLogin}>
-                Sign In
-              </button>
-              <p className="login-help-section">
-                Don't have an account? &nbsp;
-                <span onClick={() => setHasAccount(!hasAccount)}>
-                  <strong>Sign Up</strong>
-                </span>
-              </p>
-            </>
-          ) : (
-            <>
-              <button className="login-button" onClick={handleSignup}>
-                Sign Up
-              </button>
-              <p className="login-help-section">
-                Already have an account? &nbsp;
-                <span onClick={() => setHasAccount(!hasAccount)}>
-                  <strong>Sign In</strong>
-                </span>
-              </p>
-            </>
-          )}
+    <div className="login-page">
+      <div className="row">
+        <div className="col-12">
+          <div className="login-section">
+            {hasAccount ? (
+              <h4 className="login-title">Sign In</h4>
+            ) : (
+              <h4 className="login-title">Sign Up</h4>
+            )}
+            <br />
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              autoFocus
+              required
+            />
+            <p className="authentication-error-message"> {emailError} </p>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+            />
+            <p className="authentication-error-message"> {passwordError} </p>
+            <p>
+              We'll call or text you to confirm your number. Standard message
+              and data rates apply.
+            </p>
+            <div className="button-container">
+              {hasAccount ? (
+                <>
+                  <button className="login-button" onClick={handleLogin}>
+                    Sign In
+                  </button>
+                  <p className="login-help-section">
+                    Don't have an account? &nbsp;
+                    <span onClick={() => setHasAccount(!hasAccount)}>
+                      <strong>Sign Up</strong>
+                    </span>
+                  </p>
+                </>
+              ) : (
+                <>
+                  <button className="login-button" onClick={handleSignup}>
+                    Sign Up
+                  </button>
+                  <p className="login-help-section">
+                    Already have an account? &nbsp;
+                    <span onClick={() => setHasAccount(!hasAccount)}>
+                      <strong>Sign In</strong>
+                    </span>
+                  </p>
+                </>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
